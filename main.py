7645aa183df2d5ebd4cbcbfd55f49488ae107dd9
FtKuBo <semmarmehdi558@gmail.com>
2024-10-25 13:40:59 -0400

    refactor

--- main.py
+++ main.py
@@ -8,19 +8,14 @@ def getCityWeatherData(cityName):
 
     r = s.get(url, headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'})
 
-    temperature = r.html.find('span#wob_tm', first=True)
-    if not temperature is None : temperature = r.html.find('span#wob_tm', first=True).text
-    else : return None
-
-    unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first = True)
-    if not unit is None : unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first = True).text
-    else : return None
-
-    description = r.html.find('div.VQF4g', first = True).find('span#wob_dc', first=True)
-    if not description is None : description = r.html.find('div.VQF4g', first = True).find('span#wob_dc', first=True).text
-    else : return None
+    temp, unit, description = data = r.html.find('span#wob_tm', first=True), r.html.find('div.vk_bk.wob-unit span.wob_t', first = True), r.html.find('div.VQF4g', first = True).find('span#wob_dc', first=True)
     
-    return f'{cityName[0].upper()+cityName[1:]} : {temperature} {unit} | The city is {description.lower()}'
+    for i in range(len(data)):
+        if data[i]:
+            data[i] = data[i].text
+        else : return None
+    
+    return f'{cityName[0].upper()+cityName[1:]} : {temp} {unit} | The city is {description.lower()}'
 
 
 if __name__ == '__main__':
